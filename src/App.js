fcdf500174d04bd860be94ae95901f357dd84d31
Teona <teonazaalishvili2@gmail.com>
2023-04-02 12:48:52 -0400

    first commit, set up cards and rendered information

--- src/App.js
+++ src/App.js
@@ -1,11 +1,28 @@
 import './App.css';
+import { useState, useEffect } from 'react'
+import fakeFetch from './utils/fakeFetch'
+import Person from './components/Person'
 
 function App() {
+  let [people, setPeople] = useState([])
 
+  useEffect(() => {
+    const getPeople = async() => {
+      //const result = await fetch (https://example.com)
+      const result = await fakeFetch()
+      setPeople(await result.json())
+    }
+    getPeople()
+   
+  }, [])
+  
   return (
-    <div className="App">
-      PLACEHOLDER!
+    <div className="people-div">
+      <div className='d-flex flex-wrap'>
+      { people.map(person => <Person key={person.id} person={person} />) }</div>
     </div>
+    
+    
   );
 }
 
